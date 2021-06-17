03a54a35ad6111df31713d3ae8ee6804f024fe19
The1TrueJoe <jttelaak@hotmail.com>
2021-06-17 12:32:33 -0400

    Add semicolon

--- applications/server-side/director/com/jtelaa/da2/director/main.java
+++ applications/server-side/director/com/jtelaa/da2/director/main.java
@@ -2,6 +2,6 @@ package com.jtelaa.da2.director;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Distributed Automation 2")
+        System.out.println("Welcome to Distributed Automation 2");
     }
 }
