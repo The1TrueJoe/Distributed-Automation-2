781132024a68252fcdf713f3c0616f487b3fb0da
The1TrueJoe <jttelaak@hotmail.com>
2021-06-21 14:12:32 -0400

    Update lib

---
+++ DA2/src/com/jtelaa/da2/lib/config/ConfigHandler.java
@@ -0,0 +1,15 @@
+package com.jtelaa.da2.lib.config;
+
+import org.json.JSONObject;
+
+public class ConfigHandler {
+
+    JSONObject config_file;
+
+    public void newConfigFile(String path, String name) {
+
+    }
+
+    public JSONObject getConfig() { }
+
+}
--- DA2/src/com/jtelaa/da2/lib/misc/ComputerControl.java
+++ DA2/src/com/jtelaa/da2/lib/misc/ComputerControl.java
@@ -1,3 +1,9 @@
+package com.jtelaa.da2.lib.misc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ComputerControl {
 
     /**
