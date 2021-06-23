1c974ac0acbce8adde6cd47afcf484f067947b6e
The1TrueJoe <jttelaak@hotmail.com>
2021-06-23 14:28:25 -0400

    Config

---
+++ DA2/src/com/jtelaa/da2/lib/config/Config.java
@@ -0,0 +1,20 @@
+package com.jtelaa.da2.lib.config;
+
+/**
+ * Contains easily accessible methods to call the configuration file
+ * 
+ * @since 2
+ * @author Joseph
+ */
+
+public class Config {
+    
+    /** Checks config if the app will output to the local terminal */
+    public static boolean runAppVerbose() { return false; }
+
+    /** Checks config if the app will output to the log */
+    public static boolean runLogVerbose() { return false; }
+
+    /** Checks config for the sys ID */
+    public static String getID() { return "bot"; }
+}
