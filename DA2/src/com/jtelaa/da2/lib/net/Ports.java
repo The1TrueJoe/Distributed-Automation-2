30c806da6a3195880ecf9af65c5e42cfd476c474
The1TrueJoe <jttelaak@hotmail.com>
2021-06-25 13:45:59 -0400

    Improve query

--- DA2/src/com/jtelaa/da2/lib/net/Ports.java
+++ DA2/src/com/jtelaa/da2/lib/net/Ports.java
@@ -5,7 +5,9 @@ public enum Ports {
     HEARTBEAT(8679),
     CMD(8699),
     RESPONSE(8701),
-    LOG(8775);
+    LOG(8775),
+    QUERY_REQUEST(9998),
+    QUERY_RECEIVE(9999);
 
     private final int port;
 
--- DA2/src/com/jtelaa/da2/querygen/util/Query.java
+++ DA2/src/com/jtelaa/da2/querygen/util/Query.java
@@ -1,6 +1,12 @@
 package com.jtelaa.da2.querygen.util;
 
+import com.jtelaa.da2.lib.misc.Misc;
+import com.jtelaa.da2.lib.net.client.ClientUDP;
+import com.jtelaa.da2.lib.net.server.ServerUDP;
+
 public class Query {
+
+    public static final String QUERY_REQUEST_MESSAGE = "Gimme da Query!";
     
     private String unformatted_query;
     private String formatted_query; 
@@ -22,6 +28,46 @@ public class Query {
         unformatted_query = unformatQuery(formatted_query);
 
     }
+
+    /**
+     * Sends a request for a query
+     * 
+     * @return The search query
+     */
+
+    public static Query requestQuery(String ip, int request, int receive) {
+        String response = "";
+        sendRequest(ip, request);
+
+        ServerUDP get = new ServerUDP(receive);
+        get.startServer();
+        
+        while (!Misc.notBlank(response)) {
+            response = get.getMessage();
+
+        }
+
+        get.closeServer();
+        return new Query(response, false);
+
+    }
+
+    /**
+     * Sends out a request for a search query
+     * 
+     * @param ip IP address to access
+     * @param request Port for sending requests
+     */
+
+    private static void sendRequest(String ip, int request) {
+        ClientUDP send = new ClientUDP(ip, request);
+        send.startClient();
+        send.sendMessage(QUERY_REQUEST_MESSAGE);
+        send.closeClient();
+
+    }
+
+    public String toString() { return getQuery(); }
 
     public String getUnformattedQuery() { return unformatted_query; }
     public String getQuery() { return formatted_query; }
