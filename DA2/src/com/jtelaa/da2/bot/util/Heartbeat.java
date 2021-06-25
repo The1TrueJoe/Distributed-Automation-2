6a4891e035d8a5c9597833342009ee754bed6c18
The1TrueJoe <jttelaak@hotmail.com>
2021-06-25 13:47:16 -0400

    Add heartbeat

---
+++ DA2/src/com/jtelaa/da2/bot/util/Heartbeat.java
@@ -0,0 +1,35 @@
+package com.jtelaa.da2.bot.util;
+
+import com.jtelaa.da2.bot.main.Main;
+import com.jtelaa.da2.lib.net.Ports;
+import com.jtelaa.da2.lib.net.client.ClientUDP;
+
+/**
+ * Sends out a signal to show the bot is alive
+ * 
+ * @since 2
+ * @author Joseph
+ */
+
+public class Heartbeat extends Thread {
+
+    public static int HEARTBEAT_INTERVAL = 60000;
+    public static String HEARTBEAT_MESSAGE = "Alive!";
+
+    private ClientUDP heart;
+    
+    public void run() {
+        // Setup
+        heart = new ClientUDP(Main.me.getHearbeatIP(), Ports.HEARTBEAT.getPort());
+        heart.startClient();
+
+        while (run) {
+            heart.sendMessage(HEARTBEAT_MESSAGE);
+        }
+    }
+
+    private boolean run = true;
+    public synchronized void stopSender() { run = false; }
+
+
+}
