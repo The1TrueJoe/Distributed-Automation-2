ce612e3fd80d2da91ff33ec3094215d279fc4913
The1TrueJoe <jttelaak@hotmail.com>
2021-06-25 13:48:43 -0400

    Finish logging

--- DA2/src/com/jtelaa/da2/lib/log/Log.java
+++ DA2/src/com/jtelaa/da2/lib/log/Log.java
@@ -1,8 +1,11 @@
 package com.jtelaa.da2.lib.log;
 
-import com.jtelaa.da2.lib.config.Config;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.jtelaa.da2.bot.main.Main;
 import com.jtelaa.da2.lib.net.Ports;
-import com.jtelaa.da2.lib.net.client.Client;
+import com.jtelaa.da2.lib.net.client.ClientUDP;
 
 /**
  * Logging capability
@@ -19,8 +22,11 @@ public class Log {
     /** The logging verbostiy */
     public static boolean log_verbose;
 
-    private static Client logging_client;
-    private static boolean log_established;
+    public static ClientUDP logging_client;
+    public static boolean log_established = false;
+
+    public static Queue<String> logging_queue;
+    private static LogSender sender;
 
     /**
      * Load the config from the configuration file <p>
@@ -28,14 +34,24 @@ public class Log {
      */
 
     public static void loadConfig() {
-        app_verbose = Config.runAppVerbose();
-        log_verbose = Config.runLogVerbose();
+        app_verbose = Main.me.runAppVerbose();
+        log_verbose = Main.me.runLogVerbose();
 
     }
 
     public static void openClient(String logging_server_ip) {
-        logging_client = new Client(logging_server_ip, Ports.LOG.getPort());
+        if (!log_verbose) { return; } // Stop if log is not verbose
+
+        // Start logging client
+        logging_client = new ClientUDP(logging_server_ip, Ports.LOG.getPort());
         log_established = logging_client.startClient();
+
+        // Create the queue
+        logging_queue = new LinkedList<String>();
+        
+        // Start the log sender
+        sender = new LogSender();
+        sender.start();
     }
 
     /**
@@ -65,20 +81,25 @@ public class Log {
 
     /**
      * Sends logging message no matter what the verbosity is <b>
-     * Will not attempt to send unless the log is established
+     * Will not attempt to send unless the log is established <b>
+     * This adds the message to the queue
      * 
      * @param message Message to send
      */
 
     public static boolean sendLogMessage(String message) {
-        if (!log_established) { return false; }
-        message = Config.getID() + ">" + message;
-        return logging_client.sendMessage(message);
+        if (!log_established && !sender.isAlive()) { return false; }
+        logging_queue.add(message);
+        return true;
 
     }
 
     /** Closes the logging client */
 
-    public static void closeLog() { logging_client.closeClient(); }
+    public static void closeLog() { 
+        logging_client.closeClient(); 
+        sender.stopSender();
+    
+    }
 
-}
+} 
