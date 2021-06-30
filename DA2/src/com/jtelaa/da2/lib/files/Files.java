43e89cc8fb5b5efc95458fb9b2be0163e72441f3
The1TrueJoe <jttelaak@hotmail.com>
2021-06-30 14:43:15 -0400

    Object support for files

--- DA2/src/com/jtelaa/da2/lib/files/Files.java
+++ DA2/src/com/jtelaa/da2/lib/files/Files.java
@@ -1,8 +1,13 @@
 package com.jtelaa.da2.lib.files;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import com.jtelaa.da2.lib.log.Log;
@@ -16,7 +21,14 @@ import com.jtelaa.da2.lib.log.Log;
 
 public class Files {
 
-    public static String readInternalFile(String path) {
+    /**
+     * Reads a file within the classpath
+     * 
+     * @param path Path of the file
+     * @return File as a String
+     */
+
+    public synchronized static String readInternalFile(String path) {
         ArrayList<String> list = listLinesInternalFile(path);
         String out = "";
 
@@ -29,7 +41,14 @@ public class Files {
 
     }
 
-    public static ArrayList<String> listLinesInternalFile(String path) {
+    /**
+     * Reads a file within the classpath
+     * 
+     * @param path Path of the file
+     * @return File as a list of the lines
+     */
+
+    public synchronized static ArrayList<String> listLinesInternalFile(String path) {
         ArrayList<String> list = new ArrayList<String>();
         
         try {
@@ -52,15 +71,135 @@ public class Files {
         return list;
     }
 
-    public static void writeLinesInternalFile(String[] lines, String path) {
-        try {
-            
+    /**
+     * Reads a file
+     * 
+     * @param path Path of the file
+     * @return File as a String
+     */
 
+    public synchronized static String readFile(String path) {
+        ArrayList<String> list = listLinesFile(path);
+        String out = "";
+
+        for (String item : list) {
+            out += item + "\n";
+
+        }
+
+        return out;
+
+    }
+
+    /**
+     * Reads a file
+     * 
+     * @param path Path of the file
+     * @return File as a list of the lines
+     */
+
+    public synchronized static ArrayList<String> listLinesFile(String path) {
+        ArrayList<String> list = new ArrayList<String>();
+        
+        try {
+            @SuppressWarnings ({"all"})
+            File file = new File(path);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+            BufferedReader in = new BufferedReader(inputStreamReader);
+
+            for (String line; (line = in.readLine()) != null;) {
+                list.add(line);
+
+            }
+
+            in.close();
 
         } catch (Exception e) {
             Log.sendMessage(e.getMessage());
             
         }
+
+        return list;
+    }
+
+    /**
+     * Writes a serializable object to the file
+     * 
+     * @param object Object to write
+     * @param path Path of the file
+     */
+
+    public synchronized static void writeObjectToFile(Object[] object, String path) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream(path);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+
+            for (Object ob : object) {
+                objectOut.writeObject(ob);
+            }
+            
+            objectOut.close();
+
+        } catch (Exception e) {
+            Log.sendMessage(e.getMessage());
+
+        }
+    }
+
+    /**
+     * Writes a serializable object to the file
+     * 
+     * @param object Object to write
+     * @param path Path of the file
+     */
+
+    public synchronized static void writeObjectToFile(Object object, String path) {
+        writeObjectToFile(new Object[] {object}, path);
+    }
+
+    /**
+     * Reads a serializable object from a file
+     * 
+     * @param path Path of the file
+     * @return Object
+     */
+
+    public synchronized static Object readObjectFromFile(String path, int line_index) {
+        return readObjectFromFile(path).get(line_index);
+    }
+
+    /**
+     * Reads a serializable object from a file
+     * 
+     * @param path Path of the file
+     * @return Object
+     */
+
+    public synchronized static ArrayList<Object> readObjectFromFile(String path) {
+        ArrayList<Object> objects = new ArrayList<Object>();
+        
+        try {
+            FileInputStream fileIn = new FileInputStream(path);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+
+            for (Object line; (line = objectIn.readObject()) != null;) {
+                objects.add(line);
+
+            }
+            
+            objectIn.close();
+
+            return objects;
+
+        } catch (Exception e) {
+            Log.sendMessage(e.getMessage());
+
+        }
+
+        return objects;
+
     }
 
 }
---
+++ DA2/src/com/jtelaa/da2/lib/files/ObjectUtils.java
@@ -0,0 +1,56 @@
+package com.jtelaa.da2.lib.files;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Base64;
+
+public class ObjectUtils {
+
+    /**
+     * Deserializes an object encoded as a string
+     * 
+     * @param string Serialized object
+     * 
+     * @return Object
+     * 
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+
+    public static Object objfromString(String string) throws IOException, ClassNotFoundException {
+        byte[] data = Base64.getDecoder().decode(string);
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(data));
+        Object obj  = objectInputStream.readObject();
+        
+        objectInputStream.close();
+        
+        return obj;
+    }
+
+    /**
+     * Serializes an object into a string
+     * 
+     * @param obj Object to serialize
+     * 
+     * @return Object as a string
+     * 
+     * @throws IOException
+     */
+    
+    public static String objtoString(Serializable obj) throws IOException {
+        ByteArrayOutputStream byteArrayInputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayInputStream);
+
+        objectOutputStream.writeObject(obj);
+
+        objectOutputStream.close();
+
+        return Base64.getEncoder().encodeToString(byteArrayInputStream.toByteArray()); 
+    }
+    
+}
