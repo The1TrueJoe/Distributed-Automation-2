fb0637b77514e05cdd0e67d5cf4ca86723bc58f3
The1TrueJoe <jttelaak@hotmail.com>
2021-06-30 14:42:52 -0400

    Update user

--- DA2/src/com/jtelaa/da2/lib/user/User.java
+++ DA2/src/com/jtelaa/da2/lib/user/User.java
@@ -4,8 +4,12 @@ import java.io.Serializable;
 
 import com.jtelaa.da2.lib.crypto.EncryptionUtils;
 import com.jtelaa.da2.lib.crypto.SystemCrypto;
+import com.jtelaa.da2.lib.log.Log;
 
 public class User implements Serializable {
+
+    //default serialVersion id
+    private static final long serialVersionUID = 1L;
 
     private String name;
     private String password;
@@ -17,11 +21,29 @@ public class User implements Serializable {
             this.password = password;
 
         } else {
-            this.password = EncryptionUtils.encrypt(SystemCrypto.algorithm, password, SystemCrypto.key, SystemCrypto.iv);
+            try {
+                this.password = EncryptionUtils.encrypt(SystemCrypto.algorithm, password, SystemCrypto.key, SystemCrypto.iv);
+            
+            } catch (Exception e) {
+                Log.sendMessage(e.getMessage());
 
+            }
         }
+    }
 
+    // Ex: {User: oof, Password: test1234}
+    public User(String user) {
+        int user_index_st = user.indexOf("{User: ") + (("{User: ").length());
+        int user_index_end = user.indexOf(", Password: ");
+        int pswd_index_st = user_index_end + (", Password: ").length();
+
+        name = user.substring(user_index_st, user_index_end);
+        password = user.substring(pswd_index_st, user.length()-1);
     }
     
+    public String getName() { return name; }
+    public String getPassword() { return password; }
+
+    public String toString() { return "{User: " + name + ", Password: " + password + "}"; }
     
 }
--- DA2/src/com/jtelaa/da2/lib/user/UserHandler.java
+++ DA2/src/com/jtelaa/da2/lib/user/UserHandler.java
@@ -1,5 +1,107 @@
 package com.jtelaa.da2.lib.user;
 
+import java.util.ArrayList;
+
+import com.jtelaa.da2.lib.files.Files;
+import com.jtelaa.da2.lib.files.ObjectUtils;
+import com.jtelaa.da2.lib.log.Log;
+
+/**
+ * This manages the user accounts in the system
+ * 
+ * @since 2
+ * @author Joseph
+ */
+
 public class UserHandler {
+
+    public ArrayList<User> users;
+
+    public UserHandler() {
+        users = new ArrayList<User>();
+
+        
+    }
+
+    /**
+     * Gets a user at an index
+     * 
+     * @param index Index of user
+     * 
+     * @return User
+     */
+
+    public User getUser(int index) {
+        return users.get(index);
+    }
+
+    /**
+     * Adds user to pool
+     * 
+     * @param user User to add
+     */
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    /**
+     * Serializes and exports the user objects
+     * 
+     * @param file_path File path
+     */
+
+    public void exportusr(String file_path) {
+        Files.writeObjectToFile(users.toArray(), file_path);
+    }
+
+    /**
+     * Imports users from a file
+     * 
+     * @param path File path
+     */
+
+    public void importusr(String path) {
+        importusr(Files.listLinesFile(path));
+    }
+
+    /**
+     * Imports users from an internal file
+     * 
+     * @param path File path
+     */
+
+    public void importInternalusr(String path) {
+        importusr(Files.listLinesInternalFile(path));
+    }
+
+    /**
+     * Imports users from an arraylist of file lines
+     * 
+     * @param file_out List of lines
+     */
+
+    public void importusr(ArrayList<String> file_out) {
+        try {
+            for (String usr : file_out) {
+                users.add((User) ObjectUtils.objfromString(usr));
+        
+            }
+
+        } catch (Exception e) {
+            Log.sendMessage(e.getMessage());
+
+        }
+    }
+
+    /**
+     * Exports root user into file
+     */
+    public static void main(String[] args) {
+        UserHandler userHandler = new UserHandler();
+        userHandler.addUser(new User("root", "password", true));
+        userHandler.exportusr("src/com/jtelaa/da2/lib/user/RootUser.txt");
+
+    }
     
 }
