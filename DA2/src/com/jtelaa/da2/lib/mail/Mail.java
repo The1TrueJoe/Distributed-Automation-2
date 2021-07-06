80020458473be2ecaa44b2cd5d02c87a1456bc77
The1TrueJoe <jttelaak@hotmail.com>
2021-07-06 16:09:22 -0400

    email support

---
+++ DA2/src/com/jtelaa/da2/lib/mail/Mail.java
@@ -0,0 +1,120 @@
+package com.jtelaa.da2.lib.mail;
+
+import java.io.Serializable;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Mail object for organization purposes
+ * 
+ * @since 2
+ * @author Joseph
+ */
+
+public class Mail implements Serializable {
+
+    private String[] email_to;
+    private String email_from;
+
+    private String message;
+    private String subject;
+
+    /**
+     * Constructor
+     * 
+     * @param email_to Email recipient
+     * @param email_from Email sender
+     * @param subject Email subject
+     * @param message Email message
+     */
+
+    public Mail(String email_to, String email_from, String subject, String message) {
+        this.email_to = new String[] { email_to };
+        this.email_from = email_from;
+        this.subject = subject;
+        this.message = message;
+
+    }
+
+    /**
+     * Constructor
+     * 
+     * @param email_to Email recipients
+     * @param email_from Email sender
+     * @param subject Email subject
+     * @param message Email message
+     */
+
+    public Mail(String email_to[], String email_from, String subject, String message) {
+        this.email_to = email_to;
+        this.email_from = email_from;
+        this.subject = subject;
+        this.message = message;
+
+    }
+
+    /** @return Sender email */
+    public String getSender() { return email_from; }
+
+    /** @return Destination email */
+    public String[] getRecipients() { return email_to; }
+
+    /** @return Destination email (If only one recipient) */
+    public String getRecipient() { return email_to[0]; }
+
+    /** @return Subject */
+    public String getSubject() { return subject; }
+
+    /** @return Message */
+    public String getMessage() { return message; }
+
+    /**
+     * Formats the message so it can be sent
+     * 
+     * @param session Mail Session
+     * 
+     * @return Email
+     * 
+     * @throws MessagingException
+     */
+
+    public MimeMessage get(Session session) throws MessagingException {
+        MimeMessage mimeMessage = new MimeMessage(session);
+
+        mimeMessage.setFrom(new InternetAddress(email_from));
+
+        for (String recepient : email_to) {
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+        }
+
+        mimeMessage.setSubject(subject);
+        mimeMessage.setText(message);
+        
+        return mimeMessage;
+
+    }
+
+    /**
+     * Prints out the basic information in the message
+     * 
+     * @return String of summary
+     */
+
+    public String summary() {
+        return "[To: " + email_to + ", From: " + email_from + ", Subject: " + subject +"]";
+    }
+
+    public String toString() { 
+        return 
+            "To: " + email_to + ", From: " + email_from + "\n" +
+            "Subject: " + subject + "\n" +
+            "Message:\n" + 
+            message
+        ; 
+    }
+    
+}
---
+++ DA2/src/com/jtelaa/da2/lib/mail/MailSender.java
@@ -0,0 +1,112 @@
+package com.jtelaa.da2.lib.mail;
+
+import java.util.LinkedList;
+import java.util.Properties;
+import java.util.Queue;
+
+import javax.mail.Authenticator;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+
+import com.jtelaa.da2.lib.log.Log;
+import com.jtelaa.da2.lib.misc.MiscUtil;
+
+/**
+ * Program to send email notifications
+ * 
+ * <p> Example use case is gift-card code distribution
+ * 
+ * @since 2
+ * @author Joseph
+ * 
+ * @see com.jtelaa.da2.lib.mail.Mail
+ */
+
+ //TODO Comment
+
+public class MailSender extends Thread {
+
+    private String host;
+
+    private String user;
+    private String password;
+
+    private boolean isConfigured;
+
+    private Properties properties;
+    private Session session;
+
+    public static volatile Queue<Mail> mail_queue;
+
+    /**
+     * Configures SMTP
+     * 
+     * @param user User
+     * @param password Password
+     * @param host SMTP Host
+     */
+
+    public synchronized void configure(String user, String password, String host) {
+        this.user = user;
+        this.password = password;
+        this.host = host;
+
+        isConfigured = true;
+    }
+
+
+    public void run() {
+        properties = new Properties();
+        properties.put("mail.smtp.host", host);  
+        properties.put("mail.smtp.auth", "true"); 
+
+        mail_queue = new LinkedList<>();
+
+        session = Session.getDefaultInstance(properties,
+            new Authenticator(){
+                @SuppressWarnings("all")
+                protected PasswordAuthentication getPasswordAuthenication() {
+                    return new PasswordAuthentication(user, password);
+                }
+            }
+        );
+
+        if (!isConfigured) {
+            do {
+                MiscUtil.waitasec();
+                Log.sendMessage("Mail waiting for config!");
+            } while (!isConfigured);
+        }
+
+        while (run) {
+            sendMessage();
+        }
+    }
+
+    private boolean run = true;
+    public synchronized void stopSender() { run = false; }
+
+    /**
+     * Sends messages
+     */
+
+    private synchronized void sendMessage() {
+        if (mail_queue.size() == 0) { return; }
+        Mail mail = mail_queue.poll();
+        Log.sendMessage("Attempting to send mail: " + mail.summary());
+
+        if (MiscUtil.notBlank(mail.getMessage())) {
+            try {
+                Transport.send(mail.get(session));
+                Log.sendMessage("Mail sent sucessfully: " + mail.summary());
+
+            } catch (MessagingException e) {
+                Log.sendMessage("Failed " + e.getMessage());
+
+            }
+        }
+    }
+    
+}
