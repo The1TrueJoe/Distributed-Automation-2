579cb75d4939eefca62e95667401fa15410046a2
JoeTelaa0 <47280152+The1TrueJoe@users.noreply.github.com>
2021-07-14 16:17:39 -0400

    Move over to manager

--- DA2/src/com/jtelaa/da2/bw_manager/util/BWMessages.java
+++ DA2/src/com/jtelaa/da2/bw_manager/util/BWMessages.java
@@ -1,10 +1,18 @@
 package com.jtelaa.da2.bot.plugin.bw.util;
 
+/***
+ * 
+ */
+
+ // TODO Comment
+
 public enum BWMessages {
     
     ACCOUNT_REQUEST_MESSAGE("Gimme an account!"),
     ACCOUNT_REPONSE_MESSAGE("Here is da account!"),
-    QUERY_REQUEST_MESSAGE("Gimme da Query!")
+    QUERY_REQUEST_MESSAGE("Gimme da Query!"),
+    CARD_REDEMPTION_MESSAGE("Here is your card!"),
+    CARD_RESPONSE_MESSAGE("Here is the code!")
     ;
 
     private final char[] message;
