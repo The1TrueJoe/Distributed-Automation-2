33afe9d022eeb225c73322a1318a5e6f0ef289b4
The1TrueJoe <jttelaak@hotmail.com>
2021-07-21 16:52:29 -0400

    Update OS Detection

--- DA2/src/com/jtelaa/da2/lib/control/ComputerControl.java
+++ DA2/src/com/jtelaa/da2/lib/control/ComputerControl.java
@@ -8,18 +8,53 @@ import com.jtelaa.da2.lib.log.Log;
 
 import java.awt.Robot;
 
-// TODO comment
+/**
+ * Methods for interacting with the local host
+ * 
+ * @since 1
+ * @author Joseph
+ * 
+ * @see com.jtelaa.da2.lib.control.Command
+ */
 
 public class ComputerControl {
 
 	private static Robot pc;
 
-	public static String getOS() { return System.getProperty("os.name"); }
+	/**
+	 * Detects OS
+	 * 
+	 * @return OS
+	 */
+
+	public static String getOS() { 
+		// Get os
+		String os = System.getProperty("os.name"); 
+
+		// If it is a form of linux
+		if (os.toLowerCase().contains("linux")) {
+			return "Linux";
+
+		// If it is a form of windows
+		} else if (os.toLowerCase().contains("windows")) {
+			return "Windows";
+
+		// This system is most efficient on linux, so it will asssume it is running on linux if the os is unknown
+		} else {
+			return "Linux";
+
+		}
+	
+	}
 
     /**
      * Runs a CMD Command
 	 * 
+	 * @param command
+	 * 
 	 * @return Response
+	 * 
+	 * @see com.jtelaa.da2.lib.control.Command
      */
 
     public static String sendCommand(Command command) {
@@ -62,16 +97,28 @@ public class ComputerControl {
 	}
 
 	/**
+     * Runs a CMD Command
+	 * 
+	 * @param command Command to send
+	 * 
+	 * @return Response
+     */
+
+    public static String sendCommand(String command) { return sendCommand(new Command(command)); }
+
+	/**
 	 * Presses specified keys at the same time and then releases them
 	 * 
 	 * @param keys
 	 */
 
 	public static void pressKey(int[] keys) {
+		// Press all keys
         for (int key : keys) {
             pc.keyPress(key);
         }
 
+		// Release all keys
         for (int key : keys) {
             pc.keyRelease(key);
         }
