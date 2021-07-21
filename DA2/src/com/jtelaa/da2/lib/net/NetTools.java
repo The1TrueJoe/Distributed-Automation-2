c237183ecfe7f65ff0d60587dbc8894c3dbe67b9
The1TrueJoe <jttelaak@hotmail.com>
2021-07-21 16:53:12 -0400

    Add default gateway manipulation & subnet detect

--- DA2/src/com/jtelaa/da2/lib/net/NetTools.java
+++ DA2/src/com/jtelaa/da2/lib/net/NetTools.java
@@ -3,59 +3,173 @@ package com.jtelaa.da2.lib.net;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.URL;
 
+import com.jtelaa.da2.lib.control.ComputerControl;
 import com.jtelaa.da2.lib.log.Log;
 
 import org.apache.commons.validator.routines.InetAddressValidator;
+
+/**
+ * Misc networking tools
+ * 
+ * @since 2
+ * @author Joseph
+ * 
+ */
 
 // TODO comment
 
 public class NetTools {
     
+    /**
+     * Gets internal ip address
+     * 
+     * @return IP
+     */
+
     public static String getLocalIP() {
         try {
+            // Get intenal ip
             return InetAddress.getLocalHost().toString();
 
         } catch (Exception e) {
-            Log.sendMessage(e.getMessage());
+            // Send error to log
+            Log.sendMessage(e);
+
+            // Return loopback address
             return "127.0.0.1";
 
         }
     }
 
+    /**
+     * Get local subnet mask 
+     * 
+     * @return subnet in form (x.x.x.x)
+     */
+
+    public static String getSubnet() {
+        try {
+            // Look at network interface
+            InetAddress localHost = Inet4Address.getLocalHost();
+            NetworkInterface networkInterface = NetworkInterface.getByInetAddress(localHost);
+        
+            // Get subnet (/x)
+            int subnet = networkInterface.getInterfaceAddresses().get(0).getNetworkPrefixLength();
+
+            // TODO support more subnets
+            // Get subnet (x.x.x.x)
+            switch (subnet) {
+                case (24): return "255.255.255.0";
+                case (16): return "255.255.0.0";
+                case (8): return "255.0.0.0";
+                default: return "255.0.0.0";
+
+            }
+
+        } catch (Exception e) {
+            // Send error to log
+            Log.sendMessage(e);
+
+            // Return loopback address
+            return "255.0.0.0";
+
+        }
+    }
+
+    /**
+     * Check external ip address
+     * 
+     * @return external ip address
+     */
+
     public static String getExternalIP() {
         try {
+            // Use Amazon AWS
             URL whatismyip = new URL("http://checkip.amazonaws.com");
-            BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
 
+            // Read results
+            BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
             return in.readLine();
 
         } catch (Exception e) {
-            Log.sendMessage(e.getMessage());
+            // Send error to log
+            Log.sendMessage(e);
+
+            // Return local ip
             return getLocalIP();
 
         }
     }
 
+    /**
+     * Set a new default gateway
+     * 
+     * @param default_gateway New default gateway
+     */
+
+    public static void setDefaultGateway(String default_gateway) { 
+        // If linux
+        if (ComputerControl.getOS().equalsIgnoreCase("linux")) {
+            // Clear default gateway
+            ComputerControl.sendCommand("sudo route delete default gw 10.0.2.2 eth0");
+
+            // Set new defult gateway
+            ComputerControl.sendCommand("sudo route delete default gw " + default_gateway + " eth0");
+
+        } else if (ComputerControl.getOS().equalsIgnoreCase("windows")) {
+            // Get internal ip & info
+            String internal_ip = getLocalIP();
+            String net_mask = getSubnet();
+
+            // Set static ip w/ new gateway
+            ComputerControl.sendCommand("netsh int ip set address \"local area connection\" static " + internal_ip + " " + net_mask + " " + default_gateway);
+
+        }
+    }
+
+    /**
+     * Checks if address is valid
+     * 
+     * @param address IP address to check
+     * 
+     * @return address validity
+     */
+
     public static boolean isValid(String address) {
+        // Validate address
         InetAddressValidator valid = new InetAddressValidator();
         return valid.isValid(address);
         
     }
 
+    /**
+     * Check if a host is alive by pinging it
+     * 
+     * @param ip IP to ping
+     * 
+     * @return life status
+     */
+
     public static boolean isAlive(String ip) {
         try {
+            // Try pinging address
             if (isValid(ip)) {
                 return InetAddress.getByName(ip).isReachable(100);
 
             }
+
+        // Send error
         } catch (IOException e) {
-            Log.sendMessage(e.getMessage());
+            Log.sendMessage(e);
 
         }
 
+        // Default return
         return false;
         
     }
