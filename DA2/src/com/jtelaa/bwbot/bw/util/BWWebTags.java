0f8bcee9467ba7d7e9928f892a72d0d06d6aee39
The1TrueJoe <jttelaak@hotmail.com>
2021-08-05 01:05:26 -0400

    Create class to store HTML info

---
+++ DA2/src/com/jtelaa/bwbot/bw/util/BWWebTags.java
@@ -0,0 +1,21 @@
+package com.jtelaa.bwbot.bw.util;
+
+/**
+ * Class that stores the the HTML IDs for the web scraper portion of this system
+ * 
+ * @author Joseph
+ * @since 2
+ */
+
+public class BWWebTags {
+
+    /** Button ID for the redemption */
+    public static final String REDEEM_BUTTON_ID = "redeem-checkout-review-confirm";
+
+    /** Value on the redemption email (Used "value" to get data) */
+    public static final String REDEEM_EMAIL_VALUE = "email-input";
+
+    /** Rewards point balance value ID (From Bing Homepage) */
+    public static final String BALANCE_SPAN_ID = "id_rc";
+    
+}
