97507f740878fd2d09dc15206d8da252400cbed7
The1TrueJoe <jttelaak@hotmail.com>
2021-11-13 16:36:08 -0500

    Scheduler skelton for akhil branch

--- DA2/src/com/jtelaa/da2/scheduler/Main.java
+++ DA2/src/com/jtelaa/da2/scheduler/Main.java
@@ -1,8 +1,5 @@
 package com.jtelaa.da2.scheduler;
 
-import com.jtelaa.da2.lib.config.ConfigHandler;
-import com.jtelaa.da2.lib.console.ConsoleBanners;
-import com.jtelaa.da2.lib.log.Log;
 
 /**
  * 
@@ -12,35 +9,23 @@ import com.jtelaa.da2.lib.log.Log;
  // TODO Setup
 
 public class Main {
-    public static volatile ConfigHandler my_config;
 
-    /**
-     * 
-     * @param args
-     */
+    // SEE DA2/src/com/jtelaa/da2/lib/bot/BotQueries.java
 
     public static void main(String[] args) {
-        // Default configuration file location
-        String config_file_location = "config.properties";
 
-        // Check for first time setup
-        boolean first_time = false;
-        for (String arg : args) {
-            if (arg.equalsIgnoreCase("setup")) {
-                config_file_location = "com/jtelaa/da2/bot/plugin/bw/config.properties";
-                my_config = new ConfigHandler(config_file_location, true);
-                first_time = true;
-                break;
-
-            }
-        }
-
-        // Load normally if not first time
-        if (!first_time) { my_config = new ConfigHandler(config_file_location, false); }
-
-        Log.loadConfig(my_config);
-        Log.openClient(my_config.getLogIP());
-
-        Log.sendSysMessage(ConsoleBanners.otherBanner("com/jtelaa/da2/scheduler/misc/Scheduler.txt"));
     }
+
+    /**
+     * Send message to the director to enable the bot
+     * 
+     * @param id Id of bot to enable
+     */
+
+    public static void sendEnableMesage(int id) {
+
+    }
+    
+
 }
+ 
