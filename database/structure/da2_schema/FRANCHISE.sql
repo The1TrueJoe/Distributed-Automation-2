7c5af9caa4af4ee0ef7865f96750c00d58160e5c
The1TrueJoe <jttelaak@hotmail.com>
2021-11-13 19:47:15 -0500

    Franchise gets assigned dhcp

--- database/structure/da2_schema/FRANCHISE.sql
+++ database/structure/da2_schema/FRANCHISE.sql
@@ -1,7 +1,9 @@
 USE BW;
 
 CREATE TABLE FRANCHISE (
-    FranchiseNum        int(3)  NOT NULL,
-    OwnerID             int(3)  NOT NULL
+    FranchiseNum        int(3)          NOT NULL,
+    OwnerID             int(3)          NOT NULL,
+    DHCPLower           varchar(10)     NOT NULL,
+    DHCPHigher          varchar(10)     NOT NULL
 
 );
