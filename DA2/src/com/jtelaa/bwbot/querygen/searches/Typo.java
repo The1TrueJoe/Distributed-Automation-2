eccfae4a5fd0899c2662ec42dbe78740ff6dcbd6
Meeeee6623 <c.benjaminsemail1@gmail.com>
2021-11-16 11:05:31 -0500

    add wrong key typos

--- DA2/src/com/jtelaa/bwbot/querygen/searches/Typo.java
+++ DA2/src/com/jtelaa/bwbot/querygen/searches/Typo.java
@@ -16,7 +16,7 @@ public class Typo {
                 typo = wrongKey(query);
                 break;
             case 1:
-                typo = missedChars(query);
+                typo = missedChar(query);
                 break;
             case 2:
                 typo = transposedChars(query);
@@ -37,22 +37,31 @@ public class Typo {
 
     private static String wrongKey(String query) {
         String typo;
-
+        Random r = new Random();
         // Dictionary of close keys
         Dictionary<String, String> keyboard = new Hashtable<String, String>() {};
         // list of close keys, use \t and # to use .split() without regex matching
         String kbd = "1\t2,q#2\t1,q,w,3#3\t2,w,e,4#4\t3,e,r,5#5\t4,r,t,6#6\t5,t,y,7#7\t6,y,u,8#8\t7,u,i,9#9\t8,i,o,0#0\t9,o,p,-#-\t0,p#q\t1,2,w,a#w\tq,a,s,e,3,2#e\tw,s,d,r,4,3#r\te,d,f,t,5,4#t\tr,f,g,y,6,5#y\tt,g,h,u,7,6#u\ty,h,j,i,8,7#i\tu,j,k,o,9,8#o\ti,k,l,p,0,9#p\to,l,-,0#a\tz,s,w,q#s\ta,z,x,d,e,w#d\ts,x,c,f,r,e#f\td,c,v,g,t,r#g\tf,v,b,h,y,t#h\tg,b,n,j,u,y#j\th,n,m,k,i,u#k\tj,m,l,o,i#l\tk,p,o#z\tx,s,a#x\tz,c,d,s#c\tx,v,f,d#v\tc,b,g,f#b\tv,n,h,g#n\tb,m,j,h#m\tn,k,j";
         for (String tuple : kbd.split("#")) {
-            
             String[] pairs = tuple.split("\t");
-            keyboard.put(pairs[0], pairs[1]);
-            System.out.println(pairs[0] + "\t" + pairs[1]);
-            
+            keyboard.put(pairs[0], pairs[1]);            
         }
 
+        int index = r.nextInt(query.length());
 
+        String c = Character.toString(query.charAt(index));
+
+        String[] typos = keyboard.get(c).split(",");
+
+        String c2 = typos[r.nextInt(typos.length)];
+
+        typo = query.substring(0, index) + c2 + query.substring(index + 1);
 
         return typo;
+        
+    }
+
+    private static String missedChar(String query) {
         
     }
     
